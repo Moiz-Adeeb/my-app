ba06439f6425c3d004f76b73e39492890688d4ee
Moiz-Adeeb <moizadeebofficial@gmail.com>
2025-03-07 15:38:21 +0500

    finally done

---
+++ src/MyComponents/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <>
+      <div>
+        This is an about Component
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi
+          cupiditate non omnis facere voluptates esse laborum voluptas.
+          Quibusdam iusto nobis suscipit quaerat, animi unde velit rem earum
+          blanditiis minima magni commodi inventore minus veniam nulla aperiam,
+          amet similique incidunt quas eligendi modi molestiae facere aliquid
+          soluta. Explicabo similique impedit, consectetur et nemo sunt fuga non
+          obcaecati ipsa ullam officiis quae nihil sit officia error!
+        </p>
+      </div>
+    </>
+  );
+};
